0e395d55da1d49aff201b2d6e865b9145b165e8d
Pierre Fourreau <pierre.fourreau@radiofrance.com>
2021-12-02 20:26:17 +0100

    Day2

--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -10,20 +10,20 @@ private const val BLANK = " "
 fun main() {
     val resultPartOne = parseMovements(
         fileName = "src/main/input/day2_part1.txt",
-        submarine = SubmarineFactory.createSubmarine(withAim = false)
+        submarine = SubmarineFactory.create(withAim = false)
     )
     println("Result part 1 $resultPartOne")
 
     val resultPartTwo = parseMovements(
         fileName = "src/main/input/day2_part2.txt",
-        submarine = SubmarineFactory.createSubmarine(withAim = true)
+        submarine = SubmarineFactory.create(withAim = true)
     )
     println("Result part 2 $resultPartTwo")
 }
 
 class SubmarineFactory {
     companion object {
-        fun createSubmarine(withAim: Boolean): Submarine = if (withAim) {
+        fun create(withAim: Boolean): Submarine = if (withAim) {
             SubmarineV2()
         } else {
             SubmarineV1()
