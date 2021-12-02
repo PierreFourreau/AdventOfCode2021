2835356b4eb707a8fab1e8f6afac983d070e5913
Pierre Fourreau <pierre.fourreau@radiofrance.com>
2021-12-02 20:26:52 +0100

    Day2

--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -8,13 +8,13 @@ private const val UP = "up"
 private const val BLANK = " "
 
 fun main() {
-    val resultPartOne = parseMovements(
+    val resultPartOne = parseSubmarineMovements(
         fileName = "src/main/input/day2_part1.txt",
         submarine = SubmarineFactory.create(withAim = false)
     )
     println("Result part 1 $resultPartOne")
 
-    val resultPartTwo = parseMovements(
+    val resultPartTwo = parseSubmarineMovements(
         fileName = "src/main/input/day2_part2.txt",
         submarine = SubmarineFactory.create(withAim = true)
     )
@@ -78,7 +78,7 @@ class SubmarineV2(var aim: Int = 0) : Submarine(), Movement {
     }
 }
 
-private fun parseMovements(
+private fun parseSubmarineMovements(
     fileName: String,
     submarine: Submarine
 ): Int {
