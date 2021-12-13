3f8c68ccf290744bb2fd41fee497691c7772b7b7
Pierre Fourreau <fourreau.pierre@gmail.com>
2021-12-13 12:39:44 +0100

    Day 11

--- src/main/kotlin/Day11.kt
+++ src/main/kotlin/Day11.kt
@@ -6,61 +6,59 @@ import java.io.File
 fun main() {
     val lines = File("src/main/input/day11.txt").readLines()
     partOne(lines)
+    partTwo(lines)
 }
 
 private fun partOne(lines: List<String>) {
     val octopuses = parseOctopuses(lines)
     var flashesCount = 0
-    octopuses.print("Before any steps")
 
-    repeat(100) { index ->
+    repeat(100) {
         flash(octopusArray = octopuses.incrementAll(), flashesCount = flashesCount)
         flashesCount += octopuses.getFlashedCount()
         octopuses.resetFlashed()
-        octopuses.print("After step ${index + 1} with flashesCount $flashesCount")
     }
     println("Result part 1 $flashesCount")
+}
+
+private fun partTwo(lines: List<String>) {
+    val octopuses = parseOctopuses(lines)
+    val octopusesCount = octopuses.flatten().size
+    var step = 0
+
+    while (octopuses.getZeroEnergyCount() != octopusesCount) {
+        flash(octopusArray = octopuses.incrementAll())
+        octopuses.resetFlashed()
+        step++
+    }
+    println("Result part 2 $step")
 }
 
 private fun parseOctopuses(lines: List<String>): Array<Array<Octopus>> =
     lines.map { line -> line.map { Octopus(it.digitToInt()) }.toTypedArray() }.toTypedArray()
 
-private fun flash(octopusArray: Array<Array<Octopus>>, flashesCount: Int = 0): Int {
+private fun flash(octopusArray: Array<Array<Octopus>>, flashesCount: Int = 0) {
     octopusArray.mapIndexed { y, line ->
         line.mapIndexed { x, octopus ->
-            if (octopus.energy > 9 && !octopus.isFlashed) {
+            if (octopus.needToBeFlashed()) {
                 Pair(x, y).getAdjacentPoints().forEach { point ->
                     octopusArray.getOrNull(point.y)?.getOrNull(point.x)?.let { octopus ->
                         octopusArray[point.y][point.x] = octopus.increment()
                     }
                 }
                 octopus.isFlashed = true
-                flash(
-                    octopusArray = octopusArray,
-                    flashesCount = flashesCount + 1
-                )
+                flash(octopusArray = octopusArray, flashesCount = flashesCount + 1)
             }
         }
     }
-    return flashesCount
 }
 
 private fun Array<Array<Octopus>>.incrementAll() = this.map { line -> line.map { it.increment() }.toTypedArray() }.toTypedArray()
 private fun Array<Array<Octopus>>.getFlashedCount() = this.flatten().count { it.isFlashed }
+private fun Array<Array<Octopus>>.getZeroEnergyCount() = this.flatten().count { it.energy == 0 }
 private fun Array<Array<Octopus>>.resetFlashed() = this.flatten().filter { it.isFlashed }.map { octopus ->
     octopus.energy = 0
     octopus.isFlashed = false
-}
-
-private fun Array<Array<Octopus>>.print(title: String) {
-    println(title)
-    this.forEach { line ->
-        line.forEach { octopus ->
-            print(octopus.energy)
-        }
-        println()
-    }
-    println("____________________________")
 }
 
 private fun Pair<Int, Int>.getAdjacentPoints() = listOf(
@@ -79,4 +77,18 @@ private data class Octopus(var energy: Int, var isFlashed: Boolean = false) {
         energy += 1
         return this
     }
+
+    fun needToBeFlashed() = this.energy > 9 && !this.isFlashed
+}
+
+// For debug printing
+private fun Array<Array<Octopus>>.print(title: String) {
+    println(title)
+    this.forEach { line ->
+        line.forEach { octopus ->
+            print(octopus.energy)
+        }
+        println()
+    }
+    println("____________________________")
 }
